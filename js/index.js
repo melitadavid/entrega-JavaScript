4d5cab811461780d9689f5602b79d3afabdebe95
David Melita <melita.david.99@gmail.com>
2022-09-04 15:40:09 -0300

    primer entrega de js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+  
+  
+  let ingresaUnNumero = parseInt(prompt("Ingresa el numero de la tabla que quieras saber"));
+
+  
+for( let i = 1 ; i <=10; i++){
+   
+   
+
+    alert(ingresaUnNumero * i);
+}
+
+alert("fin");
